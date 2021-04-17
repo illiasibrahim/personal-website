bb32e54b1aed8eac0b97a1c24f475f50d995360b
illias <illiasibrahim6@gmail.com>
2021-04-17 22:03:11 +0530

    form responses to email enabled

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ $(document).ready(function () {
             $("#namevaliderror").hide();
             $("#namelengtherror").show();
             $("#nme").css("border", "1px solid #dc3545");
-            nameflag=0;
-            $("#send").prop('disabled',true);
+            nameflag = 0;
+            $("#send").prop('disabled', true);
         }
         else {
             if (validname.test(username)) {
                 $("#namelengtherror").hide();
                 $("#namevaliderror").hide();
                 $("#nme").css("border", "1px solid #ced4da");
-                nameflag=1;
-                if(nameflag==1 && mailflag==1 && mobileflag==1){
-                    $("#send").prop('disabled',false);
+                nameflag = 1;
+                if (nameflag == 1 && mailflag == 1 && mobileflag == 1) {
+                    $("#send").prop('disabled', false);
                 }
-                
+
             }
             else {
                 $("#namelengtherror").hide();
                 $("#namevaliderror").show();
                 $("#nme").css("border", "1px solid #dc3545");
-                nameflag=0;
-                $("#send").prop('disabled',true);
+                nameflag = 0;
+                $("#send").prop('disabled', true);
             }
         }
     })
@@ -38,18 +38,18 @@ $(document).ready(function () {
         if (validmailid.test(mailid)) {
             $("#mailvaliderror").hide();
             $("#mail").css("border", "1px solid #ced4da");
-            mailflag=1;
-            if(nameflag==1 && mailflag==1 && mobileflag==1){
-                $("#send").prop('disabled',false);
+            mailflag = 1;
+            if (nameflag == 1 && mailflag == 1 && mobileflag == 1) {
+                $("#send").prop('disabled', false);
             }
-            
+
         }
         else {
             $("#mailvaliderror").show();
             $("#mail").css("border", "1px solid #dc3545");
-            mailflag=0;
-            $("#send").prop('disabled',true);
-            
+            mailflag = 0;
+            $("#send").prop('disabled', true);
+
         }
     })
     $("#mobile").blur(function () {
@@ -58,26 +58,47 @@ $(document).ready(function () {
         if (number.toString().length != 10) {
             $("#mobilevaliderror").show();
             $("#mobile").css("border", "1px solid #dc3545");
-            mobileflag=0;
-            $("#send").prop('disabled',true);
+            mobileflag = 0;
+            $("#send").prop('disabled', true);
         } else {
             if (validnumber.test(number)) {
                 $("#mobilevaliderror").hide();
                 $("#mobile").css("border", "1px solid #ced4da");
-                mobileflag=1;
-                if(nameflag==1 && mailflag==1 && mobileflag==1){
-                    $("#send").prop('disabled',false);
+                mobileflag = 1;
+                if (nameflag == 1 && mailflag == 1 && mobileflag == 1) {
+                    $("#send").prop('disabled', false);
                 }
             }
             else {
                 $("mobilevaliderror").show();
                 $("#mobile").css("border", "1px solid #dc3545");
-                mobileflag=0;
-                $("#send").prop('disabled',true);
+                mobileflag = 0;
+                $("#send").prop('disabled', true);
             }
         }
     })
-    
 
+    $("#contact-form").submit((e) => {
+        e.preventDefault()
+        $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbzJEMg9FY47bz1O4hDUjYTIpzktOTe38cHC3GPcG_Nr2xOb9heHJqtks_1VJVzkN9eymA/exec",
+            data: $("#contact-form").serialize(),
+            method: "post",
+            success: function (response) {
+                alert("Form submitted successfully")
+                document.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error: function (err) {
+                alert("Something Error")
+
+            }
+        })
+    })
+
+    $( '#contact-form' ).each(function(){
+        this.reset();
+    });
+    
 
 })
